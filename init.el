a339745ccf86c3422ed8638077ca0df7f73b2ce7
Mads Bach Villadsen <mvilladsen@pm.me>
2026-01-17 22:58:53 +0100

    better yaml and python, enable niceties

--- init.el
+++ init.el
@@ -42,11 +42,11 @@
        deft                ; notational velocity for Emacs
        doom                ; what makes DOOM look the way it does
        doom-dashboard      ; a nifty splash screen for Emacs
-       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       ;;indent-guides     ; highlighted indent columns
+       indent-guides     ; highlighted indent columns
 
        ;; ligatures seem to cause lag issues with org-mode and latex
        ;;ligatures         ; ligatures and symbols to make your code pretty again
@@ -104,7 +104,7 @@
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        direnv
        ;;docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
@@ -171,7 +171,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +tree-sitter +lsp +cython +pyright)           ; beautiful is better than ugly
+       (python +tree-sitter +lsp +cython)           ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -187,7 +187,7 @@
        ;;swift               ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +lsp +tree-sitter)               ; the tubes
-       (yaml +lsp +tree-sitter) ; JSON, but readable
+       (yaml +lsp) ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
